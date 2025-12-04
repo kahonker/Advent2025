8692b6f6de535a729197a121b37f342308b40f56
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2025-12-04 16:34:24 -0500

    day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,3 +1,5 @@
+import copy
+
 with open("input4", "r") as f:
 	data = f.read().split("\n")
 	
@@ -8,30 +10,44 @@ sum = 0
 
 coordinates = []
 
-def count_surround(i, j):
+new_data = [[]]
+
+
+
+def count_surround(i, j, data):
 	surround = 0
 	for item in check:
-		if 0 < i+item[1] < len(data) and 0 < j+item[0] < len(data[i]):
-			if data[i+item[1]][j+item[0]] == "@":
-				coordinates.append((i+item[1], j+item[0]))
+		coordinate = (i+item[1], j+item[0])
+		if not (0 > coordinate[0] or 0 > coordinate[1] or coordinate[0] > len(data)-1 or coordinate[1] > len(data[i]) -1):
+			if data[coordinate[0]][coordinate[1]] == "@":
 				surround += 1
-			new_data = data.copy()
-			for item in coordinates:
-				new_data[item[0]][item[1]] = "x"
-			pretty_print(new_data)
-			coordinates.clear()
+
 	return surround
 
 def pretty_print(data):
 	for line in data:
 		print("".join(line))
 
-for i in range(0, len(data)):
-	for j in range(0, len(data[i])):
-		if data[i][j] == "@" and count_surround(i, j) < 4:
-			print(count_surround(i,j))
-			sum += 1
+def find_rolls(data):
+	sum = 0
+	for i in range(0, len(data)):
+		for j in range(0, len(data[i])):
+			if data[i][j] == "@" and count_surround(i, j, data) < 4:
+				new_data[i][j] = "x"
+				sum += 1
+	return sum
+
+count = 1
+overall = 0
+
+while count > 0:
+	new_data = copy.deepcopy(data)
+	print(new_data)
+	count = find_rolls(new_data)
+	overall += count
+	data = new_data
 
 
+print(data)
 
-print(sum)
+print(overall)
