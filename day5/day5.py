c2305df1f414f2e31f2cc635035519e1423563d1
kahonker <alexeyk4@nycstudents.net>
2025-12-05 08:34:34 -0500

    day5

--- day5/day5.py
+++ day5/day5.py
@@ -11,6 +11,21 @@ def check_ranges(id):
 			fresh = True
 	return fresh
 	
+def fix_range_overlaps(rang):
+	for range in ranges:
+		if range[0] <= rang[0] <= range[1]:
+			if range[0] <= rang[1] <= range[1]:
+				rang = [0,0]
+			else:
+				rang[0] = range[1] +1
+		if range[0] <= rang[1] <= range[1]:
+			rang[1] = range[0] - 1
+		if rang[0] < range[0] and rang[1] > range[1]:
+			fix_range_overlaps([rang[0], range[0]-1])
+			fix_range_overlaps([range[1]+1,rang[1]])
+			return
+	ranges.append(rang)
+	
 check_rang = True
 sum = 0
 
@@ -28,19 +43,18 @@ ranges.clear()
 sum = 0
 
 for item in data:
-	print((item))
 	if item == "":
 		break
 	rang = list(map(lambda x: int(x), item.split("-")))
-	for range in ranges:
-		if range[0] <= rang[0] <= range[1]:
-			if range[0] <= rang[1] <= range[1]:
-				rang = [0,0]
-			else:
-				rang[0] = range[1] +1
-		if range[0] 
+	fix_range_overlaps(rang)
+		
+for range in ranges:
+	if range == [0,0]: continue
+	sum += range[1] - range[0] +1
+	
+for range in ranges:
+	for rang in ranges:
+		if rang == range: print("same")
+		elif range[0] < rang[0] < range[1] or range[0] < rang[1] < range[1]: print("overlap")
 
 print(sum)
-
-print(len(ids))
-	
