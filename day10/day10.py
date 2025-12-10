d54b07f21b6259dd1b3468b17a7ccaabff62709e
kahonker <alexeyk4@nycstudents.net>
2025-12-10 08:16:57 -0500

    day10

---
+++ day10/day10.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+class Machine:
+	def __init__(self, indicator, buttons, joltages):
+		self.indicator = indicator
+		self.buttons = buttons
+		self.jotages = joltages
+		self. init_indicator = ["."] * len(self.indicator)
+	
+	def check_buttons(self)
+
+with open("input10", "r") as f:
+	
