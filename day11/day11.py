66ee40b070b80cd03b45c32fa964e3771c834c74
Alexey Khoroshenkiy <alexeyk4@nycstudents.net>
2025-12-11 14:00:09 -0500

    day11

--- day11/day11.py
+++ day11/day11.py
@@ -1,48 +1,44 @@
 with open("input11", "r") as f:
 	data = f.read().split("\n")
-
+#redoinrustsmh
 data_dict = {}	
 sum = 0
 
-def search(start_key, found_dac, found_fft, curr_list):
-	global sum
-	new_list = curr_list.copy()
+def search(start_key, found_dac, found_fft):
+	if start_key == "out":
+		if found_dac and found_fft:
+			return 1
+		return 0
+	if start_key == "dac":
+		found_dac = True
+	if start_key == "fft":
+		found_fft = True
+	start_num = 0
 	for item in data_dict[start_key]:
-		if item == "out":
-			if found_dac and found_fft:
-				print("a")
-				sum += 1
-			return
+		if (item, found_dac, found_fft) in memo.keys():
+			start_num += memo[(item, found_dac, found_fft)]
 		else:
-			if item in new_list: return
-			if item == "dac":
-				found_dac = True
-			elif item == "fft":
-				found_fft = True
-			
-			search(item, found_dac, found_fft)
-	return
+			memo[(item, found_dac, found_fft)] = search(item, found_dac, found_fft)
+			start_num += memo[(item, found_dac, found_fft)]
+	return start_num
 
+memo = {}
 def search_part1(start_key):
-	global sum
-	print(sum)
-	if start_key == "svr" or start_key == "you": 
-		sum += len(data_dict[start_key])
-	else: 
-		sum += len(data_dict[start_key]) - 1
+	if start_key == "out":
+		return 1
+	start_num = 0
 	for item in data_dict[start_key]:
-		if item == "out":
-			return
+		if item in memo.keys():
+			start_num += memo[item]
 		else:
-			search_part1(item)
-	return	
+			memo[item] = search_part1(item)
+			start_num += memo[item]
+	return start_num
 	
 for item in data:
 	line = item.split(":")
 	data_dict[line[0]] = line[1].split()
 
-search_part1("svr")
-
 #search("svr", False, False)	
 			
-print(sum)
+print(search("svr", False, False))
