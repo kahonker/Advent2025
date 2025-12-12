8b05dc4b19f57bb5d82c51b782287ab891b15b02
kahonker <alexeyk4@nycstudents.net>
2025-12-11 16:39:54 -0800

    day10

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,5 @@
-from itertools import combinations, combinations_with_replacement
+import time
+from itertools import combinations
 
 
 class Machine:
@@ -7,7 +8,7 @@ class Machine:
         self.buttons = buttons
         self.joltages = joltages
         self.init_indicator = ["."] * len(self.indicator)
-        self.init_joltage = [0] * len(self.jotages)
+        self.init_joltage = [0] * len(self.joltages)
 
     def check_buttons_indicators(self):
         for r in range(1, len(self.buttons) + 1):
@@ -20,21 +21,43 @@ class Machine:
         return 0
 
     def check_buttons_joltage(self):
-        joltage_dict = dict.fromkeys(self.joltages, [])
-        for r in range(1, 100):
-            for i in sorted(combinations_with_replacement(self.buttons, r), key=lambda j : len(j)):
-                print(i)
-                joltages = self.init_joltage.copy()
-                for button in i:
-                    for index in button:
-                        joltages[index] += 1
-                if joltages == self.jotages:
-                    print(i)
-                    return len(i)
-        return 0
+        joltage_dict = dict.fromkeys(range(len(self.joltages)), [])
+        num_button_pressed = 0
+        while self.init_joltage != self.joltages:
+            buttons_list = sorted(self.buttons, key=lambda button: self.sort_buttons(button, self.init_joltage))
+            print(buttons_list)
+            print(self.init_joltage)
+            time.sleep(0.5)
+            chosen_button = buttons_list[0]
+            num_button_pressed += 1
+            for i in chosen_button:
+                self.init_joltage[i] += 1
+                joltage_dict[i].append(chosen_button)
+            listed = self.check_list_greater(self.init_joltage, self.joltages)
+            if listed:
+                button = joltage_dict[listed[0]][0]
+                for i in button:
+                    joltage_dict[i].remove(button)
+                    self.init_joltage[i] -= 1
+                num_button_pressed -= 1
+        return num_button_pressed
+
+    def check_list_greater(self, jolts_to_check, needed_jolts):
+        return_list = []
+        for i in range(len(jolts_to_check)):
+            if jolts_to_check[i] > needed_jolts[i]:
+                return_list.append(i)
+        return return_list
 
     def sort_buttons(self, button, current_jolts):
-
+        joltage_diff = [a - b for a, b in zip(self.joltages, current_jolts)]
+        affecting = 0
+        smallest_affecting = 0
+        for index in button:
+            jolts_at_index = joltage_diff[index]
+            if joltage_diff[index] > 0: affecting += 1
+            if self.init_joltage[index] < smallest_affecting or smallest_affecting == 0: smallest_affecting = self.init_joltage[index]
+        return -affecting, len(button), -smallest_affecting
 
 with open("input10", "r") as f:
     unparsed_data = f.read().split("\n")
@@ -60,7 +83,7 @@ for i in data:
     sum += i.check_buttons_indicators()
 
 for i in data:
-    print(data.index(i))
+    print(i)
     joltage_sum += i.check_buttons_joltage()
 
 print(joltage_sum)
--- day11/day11.py
+++ day11/day11.py
@@ -1,44 +1,44 @@
 with open("input11", "r") as f:
-	data = f.read().split("\n")
+    data = f.read().split("\n")
 #redoinrustsmh
 data_dict = {}	
 sum = 0
 
 def search(start_key, found_dac, found_fft):
-	if start_key == "out":
-		if found_dac and found_fft:
-			return 1
-		return 0
-	if start_key == "dac":
-		found_dac = True
-	if start_key == "fft":
-		found_fft = True
-	start_num = 0
-	for item in data_dict[start_key]:
-		if (item, found_dac, found_fft) in memo.keys():
-			start_num += memo[(item, found_dac, found_fft)]
-		else:
-			memo[(item, found_dac, found_fft)] = search(item, found_dac, found_fft)
-			start_num += memo[(item, found_dac, found_fft)]
-	return start_num
+    if start_key == "out":
+        if found_dac and found_fft:
+            return 1
+        return 0
+    if start_key == "dac":
+        found_dac = True
+    if start_key == "fft":
+        found_fft = True
+    start_num = 0
+    for item in data_dict[start_key]:
+        if (item, found_dac, found_fft) in memo.keys():
+            start_num += memo[(item, found_dac, found_fft)]
+        else:
+            memo[(item, found_dac, found_fft)] = search(item, found_dac, found_fft)
+            start_num += memo[(item, found_dac, found_fft)]
+    return start_num
 
 memo = {}
 def search_part1(start_key):
-	if start_key == "out":
-		return 1
-	start_num = 0
-	for item in data_dict[start_key]:
-		if item in memo.keys():
-			start_num += memo[item]
-		else:
-			memo[item] = search_part1(item)
-			start_num += memo[item]
-	return start_num
-	
+    if start_key == "out":
+        return 1
+    start_num = 0
+    for item in data_dict[start_key]:
+        if item in memo.keys():
+            start_num += memo[item]
+        else:
+            memo[item] = search_part1(item)
+            start_num += memo[item]
+    return start_num
+
 for item in data:
-	line = item.split(":")
-	data_dict[line[0]] = line[1].split()
+    line = item.split(":")
+    data_dict[line[0]] = line[1].split()
 
 #search("svr", False, False)	
-			
+
 print(search("svr", False, False))
